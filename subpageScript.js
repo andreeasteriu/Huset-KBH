79e9b93cc73e6bd8ea40f77e6a15e44ab81f1780
Larisa <32453313+larisaai@users.noreply.github.com>
2018-05-03 14:52:02 +0200

    Last verion but still need some changes

--- subpageScript.js
+++ subpageScript.js
@@ -4,15 +4,29 @@ let id = urlParams.get("id");
 console.log("i want to get article: " + id);
 
 
-fetch("http://larisadom.com/wordpress/wp-json/wp/v2/events?categories=7&per_page=10" + id)
-  .then(e=>e.json())
-  .then(showSinglePost)
+fetch("http://larisadom.com/wordpress/wp-json/wp/v2/events/" + id + "/?categories=7&per_page=10")
+    .then(e => e.json())
+    .then(showSinglePost)
 
 
-function showSinglePost(aPost){
+function showSinglePost(aPost) {
+console.log(aPost)
+    document.querySelector("#singleEvent h2").textContent = aPost.title.rendered;
+    document.querySelector(".subimg").src = aPost.acf.image.sizes.medium;
+    document.querySelector("p").innerHTML = aPost.content.rendered;
 
-    document.querySelector("#singleEvent h2").textContent=aPost.title.rendered;
+ document.querySelector(".price").textContent = aPost.acf.price+ " DKK";
+
+ document.querySelector(".loc").textContent = aPost.acf.location;
+
+    if (aPost.acf.availability_ === "NOT"){
+        document.querySelector(".availability img").src= "images/no.png"
+    }
+
+
 }
+
+
 
 function goBack() {
     window.history.back();
